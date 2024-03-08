5c5974d947f64c1f0b6eb4cc48a102e96270d160
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-09 02:52:49 +0600

    feat: added auth methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,99 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtPayload, Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signupLocal(authDto: AuthDto): Promise<Tokens> {
+    const hash = await this.hashData(authDto.password);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        email: authDto.email,
+        hash,
+      },
+    });
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+    await this.updateRtHash(newUser.id, tokens.refreshToken);
+    return tokens;
+  }
+
+  async signinLocal(dto: AuthDto): Promise<Tokens> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Access Denied');
+
+    const passwordMatches = await bcrypt.compare(dto.password, user.hash);
+    if (!passwordMatches) throw new ForbiddenException('Access Denied');
+
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refreshToken);
+
+    return tokens;
+  }
+
+  async logout(userId: number) {
+    await this.prisma.user.updateMany({
+      where: {
+        id: userId,
+        hashedRt: {
+          not: null,
+        },
+      },
+      data: {
+        hashedRt: null,
+      },
+    });
+  }
+
+  async updateRtHash(userId: number, rt: string): Promise<void> {
+    const hash = await this.hashData(rt);
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRt: hash,
+      },
+    });
+  }
+
+  async getTokens(userId: number, email: string): Promise<Tokens> {
+    const jwtPayload: JwtPayload = {
+      sub: userId,
+      email: email,
+    };
+
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(jwtPayload, {
+        secret: 'at-secret',
+        expiresIn: '15m',
+      }),
+      this.jwtService.signAsync(jwtPayload, {
+        secret: 'rt-secret',
+        expiresIn: '7d',
+      }),
+    ]);
+
+    return {
+      accessToken: at,
+      refreshToken: rt,
+    };
+  }
+
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
+  }
+}

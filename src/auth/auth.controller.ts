dabe067a10f71b3db6e5f1ac8c97e10a4115cc09
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-09 02:52:30 +0600

    feat: added signup, signin, logout auth methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+import { Tokens } from './types';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/local/signup')
+  @HttpCode(HttpStatus.CREATED)
+  signupLocal(@Body() authDto: AuthDto): Promise<Tokens> {
+    return this.authService.signupLocal(authDto);
+  }
+
+  @Post('/local/signin')
+  @HttpCode(HttpStatus.OK)
+  signinLocal(@Body() authDto: AuthDto): Promise<Tokens> {
+    return this.authService.signinLocal(authDto);
+  }
+
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  logout() {
+    return this.authService.logout(2);
+  }
+}

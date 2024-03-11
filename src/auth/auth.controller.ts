8e971b960bdc7f196d4945f99db88df710c1418b
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-11 21:11:46 +0600

    feat: added refresh token controller method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,45 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
-import { Tokens } from './types';
+import { JwtPayload, Tokens } from './types';
+import { AtJwtAuthGuard, RtJwtAuthGuard } from 'src/guards';
+import { GetUser } from 'src/decorators/get-user.decorator';
+import { JwtPayloadWithRt } from './types';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('/local/signup')
+  @Post('local/signup')
   @HttpCode(HttpStatus.CREATED)
   signupLocal(@Body() authDto: AuthDto): Promise<Tokens> {
     return this.authService.signupLocal(authDto);
   }
 
-  @Post('/local/signin')
+  @Post('local/signin')
   @HttpCode(HttpStatus.OK)
   signinLocal(@Body() authDto: AuthDto): Promise<Tokens> {
     return this.authService.signinLocal(authDto);
   }
 
+  @UseGuards(AtJwtAuthGuard)
   @Post('logout')
   @HttpCode(HttpStatus.OK)
-  logout() {
-    return this.authService.logout(2);
+  logout(@GetUser() user: JwtPayload) {
+    return this.authService.logout(user.sub);
+  }
+
+  @UseGuards(RtJwtAuthGuard)
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  refreshTokens(@GetUser() user: JwtPayloadWithRt) {
+    return this.authService.refreshTokens(user.sub, user.refreshToken);
   }
 }

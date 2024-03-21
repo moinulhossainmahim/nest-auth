4aac99c7a63858f553376dd886c045aa38d36bf3
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-21 23:12:02 +0600

    feat: updated jwt auth guard

--- src/guards/at-jwt.guard.ts
+++ src/guards/at-jwt.guard.ts
@@ -1,5 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JsonWebTokenError } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class AtJwtAuthGuard extends AuthGuard('access-jwt') {}
+export class AtJwtAuthGuard extends AuthGuard('access-jwt') {
+  handleRequest(err: any, user: any, info: any, context: any, status: any) {
+    console.log(info);
+    if (info?.message === 'jwt expired') {
+      throw new ForbiddenException('Provide a valid token');
+    }
+
+    if (info instanceof JsonWebTokenError) {
+      throw new UnauthorizedException('Provide a valid token');
+    }
+
+    return super.handleRequest(err, user, info, context, status);
+  }
+}
